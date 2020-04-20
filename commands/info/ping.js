0b6f190848d3750b85ea4625dd2748b3d9eabdc9
Andrew <matyushkin.andrew@gmail.com>
2020-04-20 18:53:01 +0300

    commands info category

---
+++ commands/info/ping.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: 'ping',
+    category: 'info',
+    description: 'ping',
+    run: async (client, message, args) => {
+        const msg = await message.channel.send(`🏓 Отбиваю....`);
+
+        msg.edit(`🏓 Хуяк!`);
+    }
+}
